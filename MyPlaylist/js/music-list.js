44e6c38dfcd33db7a3f239887e4af90e477d2d1d
Sandeep Sahoo <sandeepsahoo865@gmail.com>
2022-08-20 19:05:45 +0530

    Changes

---
+++ MyPlaylist/js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+  {
+    name: "Baarish (Half Girlfriend)",
+    artist: "Ash King,Shashaa Tirupati",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Baatein Ye Kabhi Na (Khamoshiyan)",
+    artist: "Arijit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Banjaara (Ek Villain)",
+    artist: "Mohammad Irfan",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Dil Main Ho Tum (Cheat India)",
+    artist: "Armaan Malik",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Still D.R.E",
+    artist: "Dr. Dre ft Snopp Dog",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Duniyaa (Lukka Chuppi)",
+    artist: "Akhil,Dhvani Bhanushali",
+    img: "music-6",
+    src: "music-6"
+  },
+  
+];
